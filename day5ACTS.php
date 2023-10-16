6483319e05bf7f54c1a60668f683f69c0faf0cc8
Anusha Kant <s4006522@student.rmit.edu.au>
2023-10-16 11:17:48 +1100

    chnages

---
+++ day5ACTS.php
@@ -0,0 +1,9 @@
+<?php 
+$multi = array(
+    array("Manufacturer","Stock","Sold"),
+    array("Tesla", 22, 18),
+    array("Rivian", 15, 13),
+    array("Polestar", 7, 3)
+);
+
+echo $multi;
---
+++ phpday3.php
@@ -0,0 +1,4 @@
+<?php
+
+$arr = [1 => 'one', 'second' => 'two', 3.14 => 'three'];
+print_r($arr);
